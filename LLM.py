73df4958e03158d7d4516844f8257a7e688e93d4
resol <resolweru@mail.ru>
2024-06-25 14:41:57 +1000

    using embedding vectors to send data to vectorized database.

--- LLM.py
+++ LLM.py
@@ -1,8 +1,10 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables.base import RunnableSerializable
 from langchain_openai import OpenAI
-import httpx
-
+from langchain_openai import OpenAIEmbeddings
+import tiktoken
+import os
+import json
 from ConfigReader import OPENAI_API_TOKEN
 
 
@@ -10,22 +12,38 @@ class LLM(object):
     def __init__(self):
         self.__OPEN_AI_KEY: str = OPENAI_API_TOKEN
         self.__chain: RunnableSerializable[dict, str]
-
-    def __get_template(self) -> RunnableSerializable:
-        return PromptTemplate.from_template("task: {task}")
-
-    def get_response(self, TASK: str) -> str:
-        llm: OpenAI = OpenAI(
+        self.__llm: OpenAI = OpenAI(
             model_name="gpt-3.5-turbo-instruct",
             openai_api_key=self.__OPEN_AI_KEY,
             max_tokens=-1,
         )
 
-        template = self.__get_template()
+    def get_response_instagram(self, TASK: str) -> str:
+        template = PromptTemplate.from_template("task: {task}")
 
-        main_llm_chain: RunnableSerializable[dict, str] = template | llm
+        main_llm_chain: RunnableSerializable[dict, str] = template | self.__llm
         RESPONSE: str = main_llm_chain.invoke(input={
             "task": TASK
+        })
+
+        return RESPONSE
+
+    def get_tokens(self, TEXT: str):
+        enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        return enc.encode(TEXT)
+
+    def get_embeddings(self, TEXT: str):
+        os.environ["OPENAI_API_KEY"] = OPENAI_API_TOKEN
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+        query_result = embeddings.embed_query(TEXT)
+        return query_result
+
+    def get_response_warhammer(self, TASK: str, PAGE_NUM: str, CONTEXT: str) -> str:
+        template = PromptTemplate.from_template(f"task: {TASK}\npage_num: {PAGE_NUM}\ncontext: {CONTEXT}")
+
+        main_llm_chain: RunnableSerializable[dict, str] = template | self.__llm
+        RESPONSE: str = main_llm_chain.invoke(input={
+            "page_num": PAGE_NUM, "context": CONTEXT
         })
 
         return RESPONSE
--- Run.py
+++ Run.py
@@ -6,11 +6,27 @@ app = Flask(__name__)
 llm_: LLM = LLM()
 
 
-@app.route('/invoke', methods=['GET'])
-def llm():
+@app.route('/invoke/instagram/', methods=['GET'])
+def instagram_llm():
     TASK: str = request.args.get('task')
-    RESPONSE: str = llm_.get_response(TASK=TASK)
-    return RESPONSE
+    return llm_.get_response_instagram(TASK=TASK)
+
+@app.route('/invoke/tokens/', methods=['GET'])
+def tokens():
+    TEXT: str = request.args.get("text")
+    return llm_.get_tokens(TEXT=TEXT)
+
+@app.route('/invoke/embeddings/', methods=['GET'])
+def embeddings():
+    TEXT: str = request.args.get("text")
+    return llm_.get_embeddings(TEXT=TEXT)
+
+@app.route('/invoke/warhammer40k/', methods=['GET'])
+def warhammer40k_llm():
+    TASK: str = request.args.get('task')
+    CONTEXT: str = request.args.get('context')
+    PAGE_NUM: str = request.args.get('page_num')
+    return llm_.get_response_warhammer(TASK=TASK, CONTEXT=CONTEXT, PAGE_NUM=PAGE_NUM)
 
 
 app.run(port=8082, debug=True)
